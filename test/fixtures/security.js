72d60f6f31a64cacd78b852137d3b86bf845f6e0
Johannes J. Schmidt <jo@die-tf.de>
2015-08-17 15:30:24 +0200

    Add tests for commonjs usage

---
+++ test/fixtures/security.js
@@ -0,0 +1,18 @@
+module.exports = {
+  admins: {
+    names: [
+      'an-admin'
+    ],
+    roles: [
+      'an-admin-role'
+    ]
+  },
+  members: {
+    names: [
+      'a-member'
+    ],
+    roles: [
+      'a-member-role'
+    ]
+  }
+}
--- test/index.js
+++ test/index.js
@@ -25,6 +25,26 @@ test('configure from json', function(t) {
   })
 })
 
+test('configure from commonjs', function(t) {
+  clear(function() {
+    secure(url + '/' + dbname, path.join(__dirname, 'fixtures', 'security.js'), function(error, response) {
+      t.notOk(error, 'no error occured')
+      t.ok(response.ok, 'response is ok')
+      t.end()
+    })
+  })
+})
+
+test('configure from commonjs/index', function(t) {
+  clear(function() {
+    secure(url + '/' + dbname, path.join(__dirname, 'fixtures', 'security'), function(error, response) {
+      t.notOk(error, 'no error occured')
+      t.ok(response.ok, 'response is ok')
+      t.end()
+    })
+  })
+})
+
 test('nano object as url', function(t) {
   secure(db, path.join(__dirname, 'fixtures', 'security.json'), function(error, response) {
     t.notOk(error, 'no error occured')
